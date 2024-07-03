d9c353cb49e0c6a255877d2382a58d3424108931
batyr <batyrakpanbet@gmail.com>
2024-07-04 00:21:30 +0500

    Added questions data, added models -> quiz_question model, added dynamically creating widgets in Column widget.

--- lib/screens/quiz.dart
+++ lib/screens/quiz.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:quiz_app/screens/questions_screen.dart';
 import 'package:quiz_app/screens/start_screen.dart';
 
